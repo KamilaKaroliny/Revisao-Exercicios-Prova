253e12f6658815ee4434c566dc473a8fd50e5d7e
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-04-04 10:08:10 -0300

    ADD -  Contagem Regressiva da NASA

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-/*Crie um algoritmo que solicite ao usuário quantos números ele quiser somar. 
-A cada novo número digitado, o valor deve ser acumulado. Ao final, exiba o resultado total da soma.
-*/
+/*1. Somador de Números  
+Crie um algoritmo que solicite ao usuário quantos números ele quiser somar. 
+A cada novo número digitado, o valor deve ser acumulado. Ao final, exiba o 
+resultado total da soma.
+
 
 let n = parseFloat (prompt('Quantos número você deseja somar?'))
 let soma = 0;
@@ -11,3 +13,30 @@ for ( i = 0; i < n; i++) {
 }
 
 console.log("A soma dos números é: " + soma);
+*/
+
+/*2. Contagem Regressiva da NASA  
+Simule uma contagem regressiva para o lançamento de um foguete, partindo de 
+10 até 0, e ao final exiba a mensagem "Lançamento!".  
+Dica: estude a função `setInterval()` do JavaScript.
+*/
+
+intervalID = setInterval(lancamento, 1000) 
+let contagem = 10;
+function lancamento() {
+    if (contagem >= 0){
+        console.log(contagem);
+        contagem--;
+    }else{
+        console.log('Lançamento');
+        clearInterval(intervalID);
+    };
+
+}
+lancamento();
+
+//S.O.S
+
+
+
+
